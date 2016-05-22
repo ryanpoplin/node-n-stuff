9da1737a4db303a8a4737b2cd9df84621e5f8a42
Byrdann Fox <byrdannfox@gmail.com>
2016-05-21 21:03:17 -0400

    remove rt stuff

--- server.js
+++ server.js
@@ -5,31 +5,5 @@ function breakpoint(data) {
     console.log(`${data} is good...`);
     require('./test.module')(data);
 }
-// breakpoint('data');
-
-// RT Class?
-// class User {
-//     constructor() {
-//         this.name = '';
-//         this.enabled = false;
-//     }
-//     getName(user) {
-//         if (user.enabled) {
-//             return user.name;
-//         }
-//     }
-//     userRT(name) {
-//         if (!name) throw Error('no need for a name?');
-//         let newUser = new User();
-//         newUser.name = name;
-//         newUser.enabled = !newUser.enabled;
-//         return newUser;
-//     }
-// }
-// let user = new User();
-// let updatedUser = user.userRT('Ryan Poplin');
-// setInterval(() => {
-//     console.log(`${user.getName(updatedUser)}`);
-//     console.log(`${updatedUser.getName(user)}`);
-// }, 1000);
+breakpoint('data');
 
